1fcda25798f6c92a5fb49252b285361fe1d66596
leticia <leticiaavolio09@gmail.com>
2025-07-31 22:18:46 -0300

    Implementação da services | Implementação da tela list utilizando Flatlist

--- app/(home)/listLembrete.tsx
+++ app/(home)/listLembrete.tsx
@@ -1,26 +1,21 @@
-import { StyleSheet, Image, Text, View, ScrollView } from "react-native";
+import { StyleSheet, View, ScrollView, FlatList } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from "expo-router";
-import { Shadow } from "react-native-shadow-2";
 import LembreteItem from "../../components/lembreteItem.tsx/lembreteItem";
+import { getAllLembretes } from "../../services/lembrete";
 
 export default function Index() {
 
-    function  navigate(){
-        
-        router.replace('/home')
-    }
+    const lembretes = getAllLembretes();
     return (
         <ScrollView>
             <SafeAreaView style={styles.container}>
                 <View>
-                    <LembreteItem />
-                    <LembreteItem />
-                    <LembreteItem />
-                    <LembreteItem />
-                    <LembreteItem />
-                    <LembreteItem />
+                    <FlatList data={lembretes}
+                    renderItem={({item})=><LembreteItem lembrete={item}/>}
+                    keyExtractor = {item=> item.LembreteId.toString()}                    
+                    />
                 </View>
                 
             </SafeAreaView>
---
+++ services/lembrete.ts
@@ -0,0 +1,5 @@
+import { data } from "../data/index";
+
+export function getAllLembretes(){
+    return data.lembretes;
+}
